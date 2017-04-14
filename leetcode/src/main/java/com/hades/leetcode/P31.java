1ccf42afe6eb39301433198d22f78ae49d25836b
demondevilhades <597967460@qq.com>
2017-04-14 17:57:37 +0800

    change

--- leetcode/src/main/java/com/hades/leetcode/P31.java
+++ leetcode/src/main/java/com/hades/leetcode/P31.java
@@ -1,30 +1,43 @@
 package com.hades.leetcode;
 
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
 public class P31 {
     public int[] nextPermutation(int[] nums) {
-        boolean b = true;
-        for (int i = nums.length - 1; i > 0; i--) {
-            if (nums[i - 1] < nums[i]) {
-                b = false;
-                break;
-            }
-        }
-        int replace;
-        if (b) {
-            for (int i = (nums.length - 1) / 2; i >= 0; i--) {
-                replace = nums[i];
-                nums[i] = nums[nums.length - 1 - i];
-                nums[nums.length - 1 - i] = replace;
-            }
-        } else {
+        if(nums.length > 0){
+            boolean b = true;
             for (int i = nums.length - 1; i > 0; i--) {
                 if (nums[i - 1] < nums[i]) {
-                    replace = nums[i];
-                    nums[i] = nums[i - 1];
-                    nums[i - 1] = replace;
+                    b = false;
                     break;
+                }
+            }
+            int replace;
+            if (b) {
+                for (int i = (nums.length - 1) / 2; i >= 0; i--) {
+                    replace = nums[i];
+                    nums[i] = nums[nums.length - 1 - i];
+                    nums[nums.length - 1 - i] = replace;
+                }
+            } else {
+                List<Integer> list = new LinkedList<Integer>();
+                for (int i = nums.length - 1; i > 0; i--) {
+                    list.add(nums[i]);
+                    if (nums[i - 1] < nums[i]) {
+                        replace = nums[i];
+                        for (int l : list) {
+                            if(replace > l && l > nums[i - 1]){
+                                replace = l;
+                            }
+                        }
+                        
+                        replace = nums[i];
+                        nums[i] = nums[i - 1];
+                        nums[i - 1] = replace;
+                        break;
+                    }
                 }
             }
         }

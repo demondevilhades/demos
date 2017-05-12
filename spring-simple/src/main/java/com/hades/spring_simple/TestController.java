e0290e04620addc7d6735a5515ffe2c53ea9671d
demondevilhades <597967460@qq.com>
2017-05-12 15:16:42 +0800

    dev

--- spring-simple/src/main/java/com/hades/spring_simple/TestController.java
+++ spring-simple/src/main/java/com/hades/spring_simple/TestController.java
@@ -7,7 +7,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/test")
+@RestController
+@RequestMapping("/test")
 public class TestController {
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());

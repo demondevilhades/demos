1c596f09b6bc6c833f7aee0927f77fe05244239f
demondevilhades <597967460@qq.com>
2018-02-09 16:36:48 +0800

    clean sth. cannot copy of html doc

---
+++ jsouptest/src/main/resources/clean.js
@@ -0,0 +1,1 @@
+javascript:for(var i=0;i<document.all.length;i++){var o=document.all[i];var css=o.style.cssText;if(css.indexOf("DISPLAY: none")!=-1||css.indexOf("FONT-SIZE: 0px;")!=-1)o.innerText="";};eval("return 0");

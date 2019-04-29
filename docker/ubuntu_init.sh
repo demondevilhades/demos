94651d08b65709392cfcf869791eaa118407a62d
demondevilhades <demonhades@foxmail.com>
2019-04-29 14:23:18 +0800

    Create ubuntu_init.sh

---
+++ docker/ubuntu_init.sh
@@ -0,0 +1,16 @@
+
+apt update -y
+
+apt install -y openssh-server
+
+apt-get install -y vim
+
+apt-get install -y screen
+
+passwd
+
+#for tf
+#apt-get install -y python-qt4
+#apt-get install -y python3-tk
+#apt-get install -y protobuf-compiler
+#apt-get install -y tmux

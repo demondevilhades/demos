898cdc6654f864ee8e6f3feaf0ec010f67f448ad
demondevilhades <demonhades@foxmail.com>
2019-06-13 17:55:59 +0800

    Update centos.sh

--- docker/run/centos.sh
+++ docker/run/centos.sh
@@ -1,7 +1,7 @@
 
 docker run -itd \
 --privileged -e container=docker \
---name centos76-base \
+--name centos-test \
 -v /data/common:/data/common \
--p 10001:22 ^
+-p 10001:22 \
 centos:latest /usr/sbin/init
